8b0d0e694baa069d53e1e46feed571d37b0432f4
LuckySandhu112 <luckysandhu1209@gmail.com>
2025-06-16 21:54:49 +0530

    Create EvenOdd.cpp

---
+++ EvenOdd.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main()
+{
+    int value;
+    std::cin>>value;
+        {
+            if(value%2==0)
+        {
+            std::cout<<"Even";
+        }
+        else
+        {
+            std::cout<<"Odd";
+        }
+    }
+}
